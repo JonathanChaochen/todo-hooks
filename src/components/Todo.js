1bb2cca0bc44ebd1fe97bac28266fab7fab54a0f
chao chen <chaochen42@gmail.com>
2019-04-24 23:25:29 +1200

    useEffect cleanup

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 const Todo = () => {
   const [todoName, setTodoName] = useState('');
   const [todoList, setTodoList] = useState([]);
+
+  useEffect(
+    () => {
+      axios
+        .get('https://react-hooks-5e579.firebaseio.com/todos.json')
+        .then(result => {
+          console.log(result);
+          const todoData = result.data;
+          const todos = [];
+          for (const key in todoData) {
+            todos.push({ id: key, name: todoData[key].name });
+          }
+          setTodoList(todos);
+        });
+
+      // cleanup
+      return () => {
+        console.log('Cleanup');
+      };
+    },
+    [todoName]
+  );
+
+  const mouseMoveHandler = event => {
+    console.log(event.clientX, event.clientY);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', mouseMoveHandler);
+    return () => {
+      document.removeEventListener('mousemove', mouseMoveHandler);
+    };
+  }, []);
+
   const inpustChangeHandler = event => {
     setTodoName(event.target.value);
   };
@@ -32,7 +66,7 @@ const Todo = () => {
       <button type="button" onClick={todoAddHandler}>
         Add
       </button>
-      <ul> {todoList.map(todo => <li ker={todo}>{todo}</li>)}</ul>
+      <ul> {todoList.map(todo => <li key={todo.id}>{todo.name}</li>)}</ul>
     </React.Fragment>
   );
 };
